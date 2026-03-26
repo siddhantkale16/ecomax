6c82582767afc5a7a980400108b993ea93755d93
siddhantkale16 <siddhant.kale@hbwsl.com>
2026-03-26 11:34:24 +0530

    add not found state

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
+export default function NotFoundPage(){
+    return (
+        <div className="w-full flex flex-col justify-center items-center h-[90vh] gap-15">
+            <h1 className="text-6xl text-gray-800">Product Not Found </h1>
+            <Link href={"/"} ><Button className="bg-emerald-400 p-6">Return Home</Button></Link>
+        </div>
+    )
+}

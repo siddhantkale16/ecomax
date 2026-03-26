e9d7e7b5bcb21e239ae54d8fb2f8441dfc2323a6
siddhantkale16 <siddhant.kale@hbwsl.com>
2026-03-26 11:34:13 +0530

    add loader

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images:{
-    remotePatterns:[{
-      protocol:"https",
-      hostname:"fakestoreapi.com",
-    }]
-  }
+ images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // allows all hosts
+      },
+    ],
+  },
 };
 
 export default nextConfig;
